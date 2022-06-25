e076a5b757bfe5b8e1ca2298afc69f3124618fd1
LethaboMaditsi <95341072+LethaboMaditsi@users.noreply.github.com>
2022-06-25 12:53:42 +0200

    Update list.js

--- JavaScript/list.js
+++ JavaScript/list.js
@@ -49,17 +49,20 @@ window.addEventListener('load', () => {
         list_el.appendChild("task_el");
 
         input.value = "";
-//edit tasks
-        task_edit_el.addEventListener('click' ,() =>{
-            if(task_edit_el.innerText.toLocaleLowerCase() == "edit") {
-            task_input_el.removeAttribute("readonly");
-            task_input_el.focus();
-            task_edit_el.innerText = "Save";
-        } else {
-            task_input_el.setAttribute("readonly", "readonly");
-            task_edit_el.innerText = "Edit";
-        }
+        //edit tasks
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLocaleLowerCase() == "edit") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Edit";
+            }
         });
-
+        //delete tasks
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+        });
     });
 });
