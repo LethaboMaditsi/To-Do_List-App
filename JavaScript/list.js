0be622c62317a4e8f0a61f6ce6e03ed6ff86b303
LethaboMaditsi <95341072+LethaboMaditsi@users.noreply.github.com>
2022-07-01 22:50:27 +0200

    css update

--- JavaScript/list.js
+++ JavaScript/list.js
@@ -1,89 +1,25 @@
-//select everything 
-//select todo-form
-const todoForm = document.querySelector('.todo-form');
-//select the input box 
-const todoInput = document.querySelector('.todo-input');
-//select the <ul> with class="todo-items"
-const todoItemsList = document.querySelector('todo-items');
-//array which stores every todos
-let todos = [];
-
-//add an eventListener on form, and listen for submit event 
-todoForm.addEventListener('submit',
-    function (event) {
-        //prevent the page from reloading when submitting 
-        event.preventDefault();
-        addTodo(todoInput.value); //call addTodo function with input box current value
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
     }
-);
 
-//function to add todo
-function addTodo(item) {
-    //if item is not empty
-    if (item !== '') {
-        //make a todo object, which has id, name, and completed properties
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
 
-        const todo = {
-            id: Date.now(),
-            name: item,
-            completed: false
-        };
-        //then add it to todos array
-        todos.puse(todo);
-        addToLocalStorage(todos); //then store in localstorage
-        //finally clear the input box value
-        todoInput.value = '';
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
     }
-}
-//function to render given todos to screen
-function renderTodos(todos) {
-    //clear everything inside <ul> with class="todo-items"
-    todoItemsList.innerHTML = '';
-
-    //run through each item inside todos
-    todos.forEach(function (item)) {
-        //check if the item is completed 
-        const checked = item.completed ? 'checked' : null;
-
-        //make a <li> element fill it 
-        //<li> </li>
-
-        const li = document.createElement('li');
-        //<li class="item"> </li>
-
-        li.setAttribute('class', 'item');
-        li.setAttribute('data-key', item.id);
-
-        //if item is completed,add a line
-        if (item.completed == true) {
-            li.class.add('checked');
-         }
-
-         li.innerHTML =
-            <input type="checkbox" class="checkbox" ${checked} ></input>
-                ${item.name}
-                <button class="delete-button">X</button>
-             ;
-             
-             //add <li> to <ul>
-             todoItemsList.append(li);
-        });
-        
-}
-//function to add todos to locakl storage 
-function addToLocalStorage(todos) {
-    //convert the array to string an store it
-    localStorage.setItem('todos',JSON.stringify(todos));
-    //render them to screen
-    renderTodos(todo);
-}
-//get items from local storage
-function getFromLocalStorage(){
-const reference = localStorage.getItem('todos');
-//if reference exists
-
-if(reference){
-    todos =JSON.parse(reference);
-    renderTodos(todos);
-}
 }
