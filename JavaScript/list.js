be84f9f2afd62d6a7487577ec2aa7a5cbd808333
LethaboMaditsi <95341072+LethaboMaditsi@users.noreply.github.com>
2022-07-01 21:08:01 +0200

    Update list.js

--- JavaScript/list.js
+++ JavaScript/list.js
@@ -9,13 +9,61 @@ const todoItemsList = document.querySelector('todo-items');
 let todos = [];
 
 //add an eventListener on form, and listen for submit event 
-todoForm.addEventListener('submit', 
-function(event) {
-    //prevent the page from reloading when submitting 
-    event.preventDefault();
-    addTodo(todoInput.value); 
-    //call addTodo function with input box current value
-}
+todoForm.addEventListener('submit',
+    function (event) {
+        //prevent the page from reloading when submitting 
+        event.preventDefault();
+        addTodo(todoInput.value); //call addTodo function with input box current value
+    }
 );
 
 //function to add todo
+function addTodo(item) {
+    //if item is not empty
+    if (item !== '') {
+        //make a todo object, which has id, name, and completed properties
+
+        const todo = {
+            id: Date.now(),
+            name: item,
+            completed: false
+        };
+        //then add it to todos array
+        todos.puse(todo);
+        addToLocalStorage(todos); //then store in localstorage
+        //finally clear the input box value
+        todoInput.value = '';
+    }
+}
+//function to render given todos to screen
+function renderTodos(todos) {
+    //clear everything inside <ul> with class="todo-items"
+    todoItemsList.innerHTML = '';
+
+    //run through each item inside todos
+    todos.forEach(function (item)) {
+        //check if the item is completed 
+        const checked = item.completed ? 'checked' : null;
+
+        //make a <li> element fill it 
+        //<li> </li>
+
+        const li = document.createElement('li');
+        //<li class="item"> </li>
+
+        li.setAttribute('class', 'item');
+        li.setAttribute('data-key', item.id);
+
+        //if item is completed,add a line
+        if (item.completed == true) {
+            li.class.add('checked');
+         }
+
+         li.innerHTML =
+            <input type="checkbox" class="checkbox" ${checked} >
+                ${item.name}
+                <button class="delete-button">X</button>
+             ;
+             /add</input>/  
+    }
+}
